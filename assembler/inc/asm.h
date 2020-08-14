6981abd04d4755cfac75bfb48c101347ea002699
b09 <code2stack@gmail.com>
2020-08-14 19:56:43 +0100

    asm.h modified

--- assembler/inc/asm.h
+++ assembler/inc/asm.h
@@ -6,7 +6,7 @@
 /*   By: bprado <bprado@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/27 18:13:22 by bprado        #+#    #+#                 */
-/*   Updated: 2020/08/14 15:30:01 by macbook       ########   odam.nl         */
+/*   Updated: 2020/08/14 17:50:19 by macbook       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,70 @@
 # define C_MAGENTA	"\x1b[35m"
 # define C_CYAN		"\x1b[36m"
 # define C_RESET	"\x1b[0m"
+
+/*
+** op.h header provided as attachment for Corewar project
+*/
+
+#define IND_SIZE				2
+#define REG_SIZE				4
+#define DIR_SIZE				REG_SIZE
+
+
+# define REG_CODE				1
+# define DIR_CODE				2
+# define IND_CODE				3
+
+
+#define MAX_ARGS_NUMBER			4
+#define MAX_PLAYERS				4
+#define MEM_SIZE				4096
+#define IDX_MOD					512
+#define CHAMP_MAX_SIZE			768
+
+#define COMMENT_CHAR			'#'
+#define LABEL_CHAR				':'
+#define DIRECT_CHAR				'%'
+#define SEPARATOR_CHAR			','
+
+#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+
+#define NAME_CMD_STRING			".name"
+#define COMMENT_CMD_STRING		".comment"
+
+#define REG_NUMBER				16
+
+#define CYCLE_TO_DIE			1536
+#define CYCLE_DELTA				50
+#define NBR_LIVE				21
+#define MAX_CHECKS				10
+
+/*
+**
+*/
+
+typedef char	t_arg_type;
+
+#define T_REG					1
+#define T_DIR					2
+#define T_IND					4
+#define T_LAB					8
+
+/*
+**
+*/
+
+# define PROG_NAME_LENGTH		(128)
+# define COMMENT_LENGTH			(2048)
+# define COREWAR_EXEC_MAGIC		0xea83f3
+
+typedef struct		header_s
+{
+  unsigned int		magic;
+  char				prog_name[PROG_NAME_LENGTH + 1];
+  unsigned int		prog_size;
+  char				comment[COMMENT_LENGTH + 1];
+}					header_t;
+
 
 #endif
--- vm/src/main.c
+++ vm/src/main.c
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bprado <bprado@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/01/27 18:13:22 by bprado        #+#    #+#                 */
+/*   Updated: 2020/08/14 15:51:35 by macbook       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "vm.h"
-#include <stdio.h>
 
 int     main(void)
 {
 	char *str = "Corewar/VM main";
-	printf("%s\n", str);
+	ft_printf("%s\n", str);
 	return (0);
 }
