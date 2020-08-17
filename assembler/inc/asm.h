f0e0cb99b305ab6b9577cdc505319c8b9b5633eb
Flavio Miceli <flavio.armando.miceli@gmail.com>
2020-08-17 23:24:12 +0200

    Feature: only_label_chars

--- assembler/inc/asm.h
+++ assembler/inc/asm.h
@@ -52,6 +52,7 @@
 #define DIRECT_CHAR				'%'
 #define SEPARATOR_CHAR			','
 
+#define N_LABEL_CHARS			37      // needs a recount
 #define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
 
 #define NAME_CMD_STRING			".name"
@@ -91,5 +92,14 @@ typedef struct		header_s
   char				comment[COMMENT_LENGTH + 1];
 }					header_t;
 
+/*
+**  Bla
+*/
+
+typedef struct		s_info
+{
+	char            *file_name;
+    t_list          *file_content;
+}					t_info;
 
 #endif
--- assembler/src/main.c
+++ assembler/src/main.c
@@ -12,32 +12,121 @@
 
 #include "asm.h"
 
-int	file_valid(int argc, char **argv, t_info info)
+/*
+**	Checks if file is valid, reads into t_info.
+**	Stores file_name and file_content.
+**	file_name is needed to name final assembly output, ie <file_name>.cor
+**
+**	params: argc, argv, struct with general info for assembler
+**	return: TRUE or FALSE, depending on validity
+*/
+
+static int	read_file(int argc, char **argv, t_info *info)
 {
-	char 	*name;
+	char 	*str;
 	int		name_len;
 	int		fd;
 
 	if (argc != 2)
-		return (-1);
-	name = argv[1];
-	name_len = ft_strlen(name);
-	if (name_len <= 2 || name[name_len - 2] != '.' || name[name_len - 1] != 's')
-		return (-1);
-	info->file_name = ft_memdup(name, name_len - 1);
+		return (FALSE);
+	str = argv[1];
+	name_len = ft_strlen(str);
+	if (name_len <= 2 || str[name_len - 2] != '.' || str[name_len - 1] != 's')
+		return (FALSE);
+	info->file_name = ft_memdup(str, name_len - 1);
 	info->file_name[name_len - 1] = '\0';
-	fd = open(name, O_RDONLY);
-	return (fd);
+	fd = open(str, O_RDONLY);
+	while (get_next_line(fd, *str) > 0)
+		ft_lstappend(info->file_content, *str);
+	return (TRUE);
 }
 
-int	main(int argc, char **argv)
+/*
+**	Finds quotation marks in a larger string and returns substring.
+**
+**	Params: str that contains "<to_return>"
+**	Return: isolated string <to_return>
+*/
+
+static char *get_name_comment(char *str)
+{
+	char	*start;
+	char	*end;
+
+	start = ft_strchr(str, '"') + 1;
+	end = ft_strchr(start, '"');
+	name = ft_memdup(start, end - start);
+	name[end - start - 1] = '\n';
+	return (name);
+}
+
+static int	only_label_chars(char *str)
+{
+	char	label_chars[N_LABEL_CHARS];
+
+	label_chars = ft_strcpy(&label_chars, LABEL_CHARS);
+	while (str)
+	{
+		i = 0;
+		while (i < N_LABEL_CHARS)
+		{
+			if (*str == label_chars[i])
+				break ;
+			i++;
+		}
+		if (i > N_LABEL_CHARS)
+			return (FALSE);
+	}
+	return (TRUE);
+}
+
+/*
+**	If the lexicon is valid this function stores info->name and info->comment
+**
+**	Params: info struct
+**	Return: TRUE or FALSE depending on validity
+*/
+
+static int	lexicon_valid(t_info *info)
+{
+	char	*curr;
+
+	curr = info->file_content;
+	while (curr)
+	{
+		// Skip comment
+		if (curr->content[0] == COMMENT_CHAR)
+			;
+		// Check that only .name and .comment start with '.'
+		// Store name and content in info.
+		// TODO:
+		// .name and .comment allowed anywhere but top?
+		// check macros in op.h for relevant constants
+		else if (curr->content[0] == '.')
+		{
+			if (ft_memcmp(curr->content, NAME_CMD_STRING, 5) == 0)
+				info->name = get_name_comment(curr->content);
+			else if (ft_memcmp(curr->content, COMMENT_CMD_STRING, 8") == 0)
+				info->comment = get_name_comment(curr->content);
+			else
+				return (FALSE);
+		}
+		// Check that LABELS and INSTRUCTIONS only contain LABEL_CHARS
+		else if (is_label(curr->content) || is_instruction(curr->content))
+			if (only_label_chars(curr->content) == FALSE)
+				return (FALSE);
+		curr = curr->next;
+	}
+}
+
+int			main(int argc, char **argv)
 {
 	// char *str = "Assembler main";
 	// ft_printf("%s\n", str);
-	fd = file_valid(argc, argv, info);
-	if (fd == -1)
+	read_file(argc, argv, info);
+	if (read_file(argc, argv, info) == FALSE)
 		return (1);
-	lexicon_valid();
+	lexicon_valid(info);
 	syntax_valid();
 	instructions_valid();
 	params_valid();
