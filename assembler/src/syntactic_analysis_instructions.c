8420b0c54267cd7c44923dec0ec4b2d128278539
Flavio Miceli <flavio.armando.miceli@gmail.com>
2020-08-28 21:40:05 +0200

    Started on valid_instructions()

--- assembler/src/syntactic_analysis_instructions.c
+++ assembler/src/syntactic_analysis_instructions.c
@@ -12,7 +12,36 @@
 
 #include "asm.h"
 
-int				valid_instructions(t_asm *asm_obj)
+static int	skip_labels(t_token **token)
 {
-	return (1);
+	t_token	*current;
+
+	current = *token;
+	while (current->type == LABEL_TKN)
+	{
+		if (current->next && current->next->type == ENDLINE_TKN)
+			current = current->next->next;
+		else
+			return (print_error(SYNTAX_EXPECTED_NL_AFTER_LABEL));
+	}
+	*token = current;
+	return (TRUE);
+}
+
+int			valid_instructions(t_asm *asm_obj)
+{
+	t_token	*current;
+
+	current = asm_obj->token_head;
+	while (current)
+	{
+		skip_labels(&current);
+		if (!is_opcode(current->type))
+			return (print_error(SYNTAX_EXPECTED_INSTRUCTION));
+		// DO STUFF TO CHECK ARGUMENTS AND SEPARATORS
+		if (current->type != ENDLINE_TKN)
+			return (print_error(SYNTAX_EXPECTED_NL));
+		current = current->next;
+	}
+	return (TRUE);
 }
