200d8dead74f858069dd70fe7c25aa846efb991a
b09 <code2stack@gmail.com>
2020-09-02 13:29:01 +0100

    creating vm, first commit on branch

--- vm/inc/vm.h
+++ vm/inc/vm.h
@@ -6,7 +6,7 @@
 /*   By: bprado <bprado@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/27 18:13:22 by bprado        #+#    #+#                 */
-/*   Updated: 2020/08/14 15:30:08 by macbook       ########   odam.nl         */
+/*   Updated: 2020/09/02 12:58:31 by macbook       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,57 @@
 # include "ft_printf.h"
 # include <stdbool.h>
 
+/*
+**	COLORS FOR FT_PRINTF
+*/
 
-# define C_RED		"\x1b[31m"
-# define C_GREEN	"\x1b[32m"
-# define C_YELLOW	"\x1b[33m"
-# define C_BLUE		"\x1b[34m"
-# define C_MAGENTA	"\x1b[35m"
-# define C_CYAN		"\x1b[36m"
-# define C_RESET	"\x1b[0m"
+# define C_RED					"\x1b[31m"
+# define C_GREEN				"\x1b[32m"
+# define C_YELLOW				"\x1b[33m"
+# define C_BLUE					"\x1b[34m"
+# define C_MAGENTA				"\x1b[35m"
+# define C_CYAN					"\x1b[36m"
+# define C_RESET				"\x1b[0m"
+
+/*
+** op.h header provided as attachment for Corewar project
+*/
+
+# define IND_SIZE				2
+# define REG_SIZE				4
+# define DIR_SIZE				REG_SIZE
+# define REG_CODE				1
+# define DIR_CODE				2
+# define IND_CODE				3
+# define MAX_ARGS_NUMBER		4
+# define MAX_PLAYERS			4
+# define MEM_SIZE				4096
+# define IDX_MOD				512
+# define CHAMP_MAX_SIZE			768
+# define N_OPCODES				16
+# define N_LABEL_CHARS			38
+# define REG_NUMBER				16
+# define CYCLE_TO_DIE			1536
+# define CYCLE_DELTA			50
+# define NBR_LIVE				21
+# define MAX_CHECKS				10
+# define T_REG					1
+# define T_DIR					2
+# define T_IND					4
+# define T_LAB					8
+# define PROG_NAME_LENGTH		128
+# define COMMENT_LENGTH			2048
+# define COREWAR_EXEC_MAGIC		0xea83f3
+
+typedef struct					s_champs
+{
+	int							fd;
+}								t_champs;
+
+typedef struct					s_arena
+{
+	int							fd;
+}								t_arena;
+
 
 #endif
--- vm/src/main.c
+++ vm/src/main.c
@@ -6,15 +6,35 @@
 /*   By: bprado <bprado@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/27 18:13:22 by bprado        #+#    #+#                 */
-/*   Updated: 2020/08/14 15:51:35 by macbook       ########   odam.nl         */
+/*   Updated: 2020/09/02 13:13:43 by macbook       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-int     main(void)
+/*
+	read arguments
+	check if -n
+*/
+
+void			input_validation(int argc, char **argv)
 {
-	char *str = "Corewar/VM main";
+	if (argc < 3)
+		exit(); // do something
+	
+}
+
+
+int				main(int argc, char **argv)
+{
+	t_arena		arena;
+	t_champs	champs;
+
+	if (argc < 3)
+		exit(1);
+	input_validation(argc, argv);
+	char *str = argv[1]; // fake line to compile
+	str = "Corewar/VM main";
 	ft_printf("%s\n", str);
 	return (0);
 }
