70b530421c057bdd925f712a9042f95e06bbc7c9
b09 <code2stack@gmail.com>
2020-09-09 19:48:40 +0100

    incorporated Flavios get_arg_size() into populate_arguments()

--- vm/src/get_arguments.c
+++ vm/src/get_arguments.c
@@ -6,7 +6,7 @@
 /*   By: bprado <bprado@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/27 18:13:22 by bprado        #+#    #+#                 */
-/*   Updated: 2020/09/09 18:59:47 by macbook       ########   odam.nl         */
+/*   Updated: 2020/09/09 19:46:48 by macbook       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 /*
 **	returns size of argument based on encoding byte, and position being checked
-**	in encoding_byte (the offset) and, if argument is a T_DIR, also based on
+**	in encoding_byte (the arg_idx) and, if argument is a T_DIR, also based on
 **	dir_two.
 */
 
-char		read_encoding_byte(char encoding_byte, int offset, bool dir_two)
+static int			get_arg_size(char encode_byte, int arg_idx, bool dir_is_two)
 {
-	if(encoding_byte == REG_CODE)// bitwise)
-		return (SIZE_REG);
-	if(encoding_byte == DIR_CODE && dir_two)// bitwise)
-		return (SIZE_SDIR);
-	if(encoding_byte == DIR_CODE && dir_two == 0)// bitwise)
-		return (SIZE_LDIR);
-	if(encoding_byte == IND_CODE)// bitwise)
-		return (SIZE_IND);
-	return (0);
+	unsigned char	mask;
+	int				shift;
+
+	shift = (6 - (arg_idx * 2));
+	mask = 0x03;
+	mask <<= shift;
+	if (encode_byte & mask == REG_CODE << shift)
+		return (1);
+	else if (dir_is_two == FALSE && encode_byte & mask == (DIR_CODE << shift))
+		return (4);
+	return (2);
 }
 
 /*
@@ -41,29 +43,28 @@ char		read_encoding_byte(char encoding_byte, int offset, bool dir_two)
 **	negative
 */
 
-int			populate_arguments(unsigned char *field, int position,\
-			unsigned char **args, bool dir_is_two)
+int					populate_arguments(unsigned char *field, int pos,\
+					unsigned char **args, bool dir_is_two)
 {
-	int		i;
-	int		error;
+	int				i;
+	int				error;
 
 	i = 0;
 	error = 1;
 	while (i < 3)
 	{
-		args[i][0] = read_encoding_byte(field[position % MEM_SIZE], i,\
-												dir_is_two);
+		args[i][0] = get_arg_size(field[(pos + 1) % MEM_SIZE], i, dir_is_two);
 		++i;
 	}
 	i = 0;
-	position++;
+	pos += 2;
 	while(i < 3)
 	{
-		ft_memcpy((void*)args[i][1], field[position % MEM_SIZE], args[i][0]);
+		ft_memcpy((void*)args[i][1], field[pos % MEM_SIZE], args[i][0]);
 		if (args[i][0] == REG_SIZE && (args[i][1] < 0 || args[i][1] > 15))
 			error = -1;
-		position += args[i][0];
+		pos += args[i][0];
 		i++;
 	}
-	return (position * error);
+	return (pos * error);
 }
