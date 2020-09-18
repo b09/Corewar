43fc6b0ee426f117505cd2b61cf3ad201788d7e7
Flavio Miceli <flavio.armando.miceli@gmail.com>
2020-09-18 19:04:42 +0200

    Working on champions

--- simple_champ.s
+++ simple_champ.s
@@ -1,13 +1,12 @@
 .name "Simple"
 .comment ""
 
-	st		r1, r2
-	sti		r2, %:live, %1
+	sti		r1, %:live, %1
+	sub		r1, r1, r2
+fork:
+	fork	%5
+	fork	%0
+	zjmp	%:fork
 live:
 	live	%0
-	ld		%12, r3
-	ld		%10, r4
-	ldi		r3, r4, r5
-	and		r3, r4, r5
-	#sub		r4, r4, r4
 	zjmp	%:live
---
+++ zork.s
@@ -0,0 +1,8 @@
+.name "ZORK"
+.comment ""
+
+	sti		r1, %:live, %1
+	and		r1, %0, r1
+live:
+	live	%1
+	zjmp	%:live
